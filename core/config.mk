3559e2484e4346365e2e1660ad2390c72dfb04c8
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add VoltageOS build support
    Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -294,7 +294,9 @@ include $(BUILD_SYSTEM)/envsetup.mk
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
+ifneq ($(VOLTAGE_BUILD),)
 include vendor/voltage/config/BoardConfigVoltage.mk
+endif
 
 # The build system exposes several variables for where to find the kernel
 # headers:
