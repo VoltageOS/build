473c2b989d01995d1e0a8107b2b2b97644fbf485
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-01-17 00:36:22 -0200

    build: Prefer using "user" buildtype prop for userdebug aswell
    * Workaround for some banking apps wrongly telling that we are rooted
    
    Change-Id: I96ef74a6d69cd44867b22bfbd0e788aabdf21662
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/sysprop_config.mk
+++ core/sysprop_config.mk
@@ -146,7 +146,10 @@ ADDITIONAL_VENDOR_PROPERTIES += \
 
 endif
 
-user_variant := $(filter user userdebug,$(TARGET_BUILD_VARIANT))
+user_variant := user
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+user_variant :=
+endif
 
 config_enable_uffd_gc := \
   $(firstword $(OVERRIDE_ENABLE_UFFD_GC) $(PRODUCT_ENABLE_UFFD_GC) default)
