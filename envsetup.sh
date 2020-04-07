95a36dd580ac7e848f7ec490f865c6ba670f6fb3
Anay Wadhera <awadhera@berkeley.edu>
2020-04-06 18:29:07 -0700

    build: properly call roomservice
    Change-Id: I64dc9eea55e1e244abbec4a57c3a33ac01edbd52
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -661,7 +661,7 @@ function lunch()
         if [[ $NO_ROOMSERVICE == true ]]; then
             echo "Roomservice turned off, type in 'export NO_ROOMSERVICE=false' if you want it back on"
         else
-            vendor/voltage/build/tools/roomservice.py $product
+            python3 vendor/voltage/build/tools/roomservice.py $product
         fi
         popd > /dev/null
         check_product $product
