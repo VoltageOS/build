cfa4f3d2d0a3cb3b14d9ac62f70284404e67ad40
Daniel Micay <danielmicay@gmail.com>
2021-10-30 09:20:51 -0400

    use GrapheneOS Camera app
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 # /product packages
 PRODUCT_PACKAGES += \
     Calendar \
-    Camera2 \
+    Camera \
     Contacts \
     DeskClock \
     Gallery2 \
