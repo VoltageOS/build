e163e5a66867fcbcb37d882309d62f03d2ef5d3b
KreAch3R <kreach3r@users.noreply.github.com>
2021-11-04 15:51:52 +0000

    build: Show useful information after flashing [SQUASH]
    * Credits to zephiK and Chroma for the original code
    * Fixed Spacing
    * Used krexus-caf/build@5c602f2 commit from krexus,
    
    * Citrus-CAF Edits:
      * Show device name next to ROM's name
      * Fix for Devices without ro.product.model prop from Citrus-CAF/build@cdcb576
      * Adapted to Android 9 PIE GetProperty logic from PixelExperience-P/build@800d29c
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- tools/releasetools/non_ab_ota.py
+++ tools/releasetools/non_ab_ota.py
@@ -204,6 +204,24 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
 
+  date = target_info.GetBuildProp("ro.voltage.build.date")
+  version = target_info.GetBuildProp("ro.voltage.version")
+
+  if target_info.GetBuildProp("ro.product.model") is not None:
+    model = target_info.GetBuildProp("ro.product.model")
+    script.Print("***********************************************");
+    script.Print("           VOLTAGE-OS for %s"%(model));
+    script.Print("   Version: %s"%(version));
+    script.Print("   Compiled on: %s"%(date));
+    script.Print("***********************************************");
+  else:
+    name = target_info.GetBuildProp("ro.product.name")
+    script.Print("***********************************************");
+    script.Print("           VOLTAGE-OS for %s"%(name));
+    script.Print("   Version: %s"%(version));
+    script.Print("   Compiled on: %s"%(date));
+    script.Print("***********************************************");
+
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
 
