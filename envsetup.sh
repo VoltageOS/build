e713c3b9a021ea6e7c5c72545f82d8a01a5313cc
Daniel Micay <danielmicay@gmail.com>
2022-09-09 05:14:49 -0400

    use arm64 for default target instead of arm
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -624,7 +624,7 @@ function chooseproduct()
     if [ "x$TARGET_PRODUCT" != x ] ; then
         default_value=$TARGET_PRODUCT
     else
-        default_value=aosp_arm
+        default_value=aosp_arm64
     fi
 
     export TARGET_BUILD_APPS=
@@ -786,7 +786,7 @@ function lunch()
         answer=$1
     else
         print_lunch_menu
-        echo "Which would you like? [aosp_arm-eng]"
+        echo "Which would you like? [aosp_arm64-eng]"
         echo -n "Pick from common choices above (e.g. 13) or specify your own (e.g. aosp_barbet-eng): "
         read answer
         used_lunch_menu=1
@@ -796,7 +796,7 @@ function lunch()
 
     if [ -z "$answer" ]
     then
-        selection=aosp_arm-eng
+        selection=aosp_arm64-eng
     elif (echo -n $answer | grep -q -e "^[0-9][0-9]*$")
     then
         local choices=($(TARGET_BUILD_APPS= get_build_var COMMON_LUNCH_CHOICES))
@@ -947,7 +947,7 @@ function tapas()
         return
     fi
 
-    local product=aosp_arm
+    local product=aosp_arm64
     case $arch in
       x86)    product=aosp_x86;;
       arm64)  product=aosp_arm64;;
