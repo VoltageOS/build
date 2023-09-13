46f6f337cb49753963ca0102c640379c84b5dd48
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-09-13 10:53:50 +0300

    add VoltageOS build environment setup
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -15,6 +15,11 @@
 # gettop is duplicated here and in shell_utils.mk, because it's difficult
 # to find shell_utils.make without it for all the novel ways this file can be
 # sourced.  Other common functions should only be in one place or the other.
+
+umask 022
+# use shell built-in instead of the separate program which is missing by default on some distros
+alias which='command -v'
+
 function _gettop_once
 {
     local TOPFILE=build/make/core/envsetup.mk
@@ -1203,6 +1208,14 @@ validate_current_shell
 set_global_paths
 source_vendorsetup
 addcompletions
+
+export LANG=C.UTF-8
+export BUILD_DATETIME=${BUILD_DATETIME:-$(cat ${OUT_DIR:-out}/build_date.txt 2>/dev/null || date -u +%s)}
+export BUILD_NUMBER=${BUILD_NUMBER:-$(cat ${OUT_DIR:-out}/soong/build_number.txt 2>/dev/null || date -u -d @$BUILD_DATETIME +%Y%m%d00)}
+export BUILD_USERNAME=android-user
+export BUILD_HOSTNAME=r-0123456789abcdef-0123
+
+echo "export BUILD_DATETIME=$BUILD_DATETIME BUILD_NUMBER=$BUILD_NUMBER"
 
 # check and set ccache path on envsetup
 if [ -z ${CCACHE_EXEC} ]; then
