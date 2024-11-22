5029132481a74b2627030104d035f8fd82236e41
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-11-22 12:47:08 +0200

    add NetworkLocation
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- target/product/handheld_system.mk
+++ target/product/handheld_system.mk
@@ -59,6 +59,7 @@ PRODUCT_PACKAGES += \
     MmsService \
     MtpService \
     MusicFX \
+    NetworkLocation \
     PacProcessor \
     preinstalled-packages-platform-handheld-system.xml \
     PrintRecommendationService \
